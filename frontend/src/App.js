c47249389c97460cb0e111dbed5e0a91e13e0b1c
shalinisheetal <shalinisheetal2001@gmail.com>
2021-09-10 15:01:36 +0530

    Add react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
-  return <h1>Welcome to ProShop</h1>;
+  return (
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
