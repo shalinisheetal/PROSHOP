742d0569afc425e4d7df28df1b343bd5f9e2a1d4
shalinisheetal <shalinisheetal2001@gmail.com>
2022-01-05 11:39:15 +0530

    Delete product/Admin

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,6 +17,7 @@ const getProductById = asyncHandler(async (req, res) => {
   // find particular product in model by id
   const product = await Product.findById(req.params.id);
 
+  //  if product exists
   if (product) {
     res.send(product);
   } else {
@@ -25,6 +26,21 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product removed." });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export { getProducts, getProductById, deleteProduct };
 
 // asynHandler is a middleware used
--- frontend/src/Screens/ProductListScreen.js
+++ frontend/src/Screens/ProductListScreen.js
@@ -4,13 +4,20 @@ import { Table, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { listProducts } from "../actions/productActions";
+import { listProducts, deleteProduct } from "../actions/productActions";
 
 const ProductListScreen = ({ history, match }) => {
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
+
+  const productDelete = useSelector((state) => state.productDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = productDelete;
 
   // to check whether user is logged in
   const userLogin = useSelector((state) => state.userLogin);
@@ -22,14 +29,14 @@ const ProductListScreen = ({ history, match }) => {
     } else {
       history.push("/login");
     }
-  }, [dispatch, history, userInfo]);
+  }, [dispatch, history, userInfo, successDelete]);
 
   const createProductHandler = (product) => {
     // CREATE PRODUCT
   };
   const deleteHandler = (id) => {
     if (window.confirm("Are you sure?")) {
-      //dispatch(deleteProducts(id));
+      dispatch(deleteProduct(id));
     }
   };
 
@@ -45,6 +52,8 @@ const ProductListScreen = ({ history, match }) => {
           </Button>
         </Col>
       </Row>
+      {loadingDelete && <Loader />}
+      {errorDelete && <message variant="danger">{errorDelete}</message>}
       {loading ? (
         <Loader />
       ) : error ? (
