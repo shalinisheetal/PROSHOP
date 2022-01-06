8aecfc199ef77143446dcf754d246a6f13013f79
shalinisheetal <shalinisheetal2001@gmail.com>
2022-01-07 00:41:40 +0530

    Implement search box

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,8 +5,17 @@ import Product from "../models/productModel.js";
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+
   // find all products
-  const products = await Product.find({});
+  const products = await Product.find({ ...keyword });
   res.json(products);
 });
 
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -21,27 +21,29 @@ import {
 } from "../constants/productConstants";
 
 // thunk allows function inside function
-export const listProducts = () => async (dispatch) => {
-  try {
-    dispatch({ type: PRODUCT_LIST_REQUEST });
+export const listProducts =
+  (keyword = "") =>
+  async (dispatch) => {
+    try {
+      dispatch({ type: PRODUCT_LIST_REQUEST });
 
-    const { data } = await axios.get("/api/products");
+      const { data } = await axios.get(`/api/products?keyword=${keyword}`);
 
-    // if we obtain a successful result else trigger catch
-    dispatch({
-      type: PRODUCT_LIST_SUCCESS,
-      payload: data,
-    });
-  } catch (error) {
-    dispatch({
-      type: PRODUCT_LIST_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    });
-  }
-};
+      // if we obtain a successful result else trigger catch
+      dispatch({
+        type: PRODUCT_LIST_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_LIST_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
 
 export const listProductDetails = (id) => async (dispatch) => {
   try {
